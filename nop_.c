e3c7186b747021d65c1528d43653ba2dae04b87e
Y-admin <yassminash305@gmail.com>
2023-12-14 19:04:53 +0200

    1

---
+++ nop_.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop_ -  doesn’t do anything.
+ * @stack: pointer to lists for monty stack
+ * @line_number: number of line opcode occurs on
+*/
+void nop_(__attribute__ ((unused))stack_t **stack,
+		__attribute__ ((unused)) unsigned int line_number)
+{
+	;
+}
